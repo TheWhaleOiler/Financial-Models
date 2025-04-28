Fixed formula errors

monte_carlo_sim.py
@@ -5,13 +5,21 @@ from library.stock import Stock
 from library.price_data import daily_timeseries
 import seaborn as sns
 
-ticker = 'tsla'
+########################################
+# Monte Carlo Simulation of Stock Prices
+# Leveraged vs Regular
+########################################
+
+ticker = 'spy'
 data = daily_timeseries(ticker)
 
 stock = Stock(data['Time Series (Daily)'])
 
-annual_vol = stock.annual_volatility(timeframe_years=5)
-annual_return = stock.annual_return(timeframe_years=5)
+######################################
+# PARAMETERS
+years = 1             # Time in years
+annual_vol = stock.annual_volatility(timeframe_years=years)
+annual_return = stock.annual_return(timeframe_years=years)
 
 print(f"Annual Return: {annual_return:.2%}")
 print(f"Annual Volatility: {annual_vol:.2%}")
@@ -19,12 +27,13 @@ print(f"Annual Volatility: {annual_vol:.2%}")
 S0 = 100              # Initial price
 mu = annual_return    # Expected return (annual)
 sigma = annual_vol    # Volatility (annual)
-T = 5                 # Time in years
-steps = 252 * T       # Trading days in a year
+steps = 252 * years   # Trading days in a year
 dt = 1/252            # Time step (in years)
-n = 1000               # Number of simulations
+n = 1000              # Number of simulations
+leverage = 3          # Leverage factor
 
-leverage = 2          # Leverage factor
+# END PARAMETERS
+#######################################
 
 price_paths = np.zeros((n, steps))
 price_paths_lev = np.zeros((n, steps))
@@ -37,7 +46,7 @@ for t in range(1, steps):
 
 for t in range(1, steps):
     z = np.random.normal(0, 1, n)
-    price_paths_lev[:, t] = price_paths_lev[:, t-1] * np.exp( leverage * (mu - 0.5 * sigma**2) * dt + sigma * np.sqrt(dt) * z)
+    price_paths_lev[:, t] = price_paths_lev[:, t-1] * np.exp( leverage * ((mu - 0.5 * sigma**2) * dt + sigma * np.sqrt(dt) * z))
 
 mean_regular = np.mean(price_paths[:, -1])
 mean_leveraged = np.mean(price_paths_lev[:, -1])